614f9d41cbb8eb07eb215b6beb7f0f7ac09bbcf8
Chris Yang <chrisyx511@gmail.com>
2025-09-16 22:09:35 -0400

    Updated style

--- demo/slides/animations.inc.js
+++
@@ -1,9 +0,0 @@
-var svg = slide.getElementsByTagName('svg')[0];
-
-slide.onSlideAppears = function() {
-	svg.unpauseAnimations();
-};
-
-slide.onSlideDisappears = function() {
-	svg.pauseAnimations();
-};
